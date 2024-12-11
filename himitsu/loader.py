62a3f772d53e250f58a33e01189aadf7e7bb800e
wamo <ainznino@pm.me>
2024-12-11 12:02:37 +0900

    latest

--- himitsu/loader.py
+++ himitsu/loader.py
@@ -1,23 +1,18 @@
 from typing import List, Tuple
 
 import transformers
-from transformers import AutoModelForCausalLM, AutoTokenizer, T5Tokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
-def load_model(language: str, device: str = "cpu", model_name: str = "sbintuitions/sarashina2-7b") -> transformers.PreTrainedModel:
-    if language == "en":
-        model = AutoModelForCausalLM.from_pretrained(model_name)
-    elif language == "ja":
-        model = AutoModelForCausalLM.from_pretrained(model_name)
-    else:
-        raise ValueError(f"Invalid language: {language}")
+def load_model(language: str, device: str = "cpu", model_name: str = "leia-llm/Leia-Swallow-7b") -> transformers.PreTrainedModel:
+    model = AutoModelForCausalLM.from_pretrained(model_name)
     model.to(device)
     model.eval()
     return model
 
 
 def load_tokenizer(language: str, special_tokens: list = ["<unk>", "<s>", "</s>", "[PAD]", "[CLS]", "[SEP]", "[MASK]", "▁", "<0x0A>"],
-                   tokenizer_name: str = "sbintuitions/sarashina2-7b") -> Tuple[transformers.PreTrainedTokenizer, List[str]]:
+                   tokenizer_name: str = "leia-llm/Leia-Swallow-7b") -> Tuple[transformers.PreTrainedTokenizer, List[str]]:
     if language == "en":
         tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
     elif language == "ja":
--- local_api.py
+++ local_api.py
@@ -5,7 +5,7 @@ import himitsu
 app = FastAPI()
 
 models = [
-    "sbintuitions/sarashina2-7b",
+    "leia-llm/Leia-Swallow-7b",
     "llm-jp/llm-jp-3-1.8b",
     "leia-llm/Leia-Swallow-7b",
     "rinna/youri-7b",
@@ -15,7 +15,7 @@ models = [
 
 @app.get("/encode")
 async def encode(secret: Annotated[str, Query(description="秘密文。ビット列である必要があります")], prompt: Annotated[str, Query(description="生成プロンプト")], min_prob: float = 0.01, device=Query("cpu", enum=["cpu", "cuda:0", "mps"], description="使用するデバイス"),
-                 language=Query("ja", enum=["en", "ja"], description="文章の言語"), model_name=Query("sbintuitions/sarashina2-7b", enum=models, description="言語モデル（Hugging Face）")):
+                 language=Query("ja", enum=["en", "ja"], description="文章の言語"), model_name=Query("leia-llm/Leia-Swallow-7b", enum=models, description="言語モデル（Hugging Face）")):
     model = himitsu.load_model(language, device, model_name=model_name)
     tokenizer, special_tokens = himitsu.load_tokenizer(
         language, tokenizer_name=model_name)
@@ -32,7 +32,7 @@ async def encode(secret: Annotated[str, Query(description="秘密文。ビット
 
 @app.get("/decode")
 async def decode(cover_text: Annotated[str, Query(description="デコードする文章")], prompt: Annotated[str, Query(description="生成プロンプト")], min_prob: float = 0.01,  device=Query("cpu", enum=["cpu", "cuda:0", "mps"], description="使用するデバイス"),
-                 language=Query("ja", enum=["en", "ja"], description="文章の言語"), model_name=Query("sbintuitions/sarashina2-7b", enum=models, description="言語モデル（Hugging Face）")):
+                 language=Query("ja", enum=["en", "ja"], description="文章の言語"), model_name=Query("leia-llm/Leia-Swallow-7b", enum=models, description="言語モデル（Hugging Face）")):
     model = himitsu.load_model(language, device, model_name=model_name)
     tokenizer, special_tokens = himitsu.load_tokenizer(
         language, tokenizer_name=model_name)
